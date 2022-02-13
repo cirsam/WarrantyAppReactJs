17e06488f23add46df3467406eee6c4a6c515a2d
Samuel Antwi <cirsam@hotmail.com>
2022-02-13 08:21:29 -0500

    Added signup changes to login system

---
+++ src/EndPointServices/signup.js
@@ -0,0 +1,20 @@
+import ApiService from "./apiService";
+
+async function handleSignup (userCredentials){
+    console.log(userCredentials);
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',"Access-Control-Allow-Origin": "*", 'Accept': 'application/json','content-type':'application/text'},
+        body: JSON.stringify({
+            Username:userCredentials.username,
+            Email:userCredentials.email,
+            Password:userCredentials.password,
+            ConfirmPassword:userCredentials.confirmPassword
+        }),
+        mode:'cors'
+    };
+    
+    return ApiService('https://localhost:44357/api/login/register',requestOptions);
+}
+
+export default handleSignup;
